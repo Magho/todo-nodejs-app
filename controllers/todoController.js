11eae6e1e9c94719a8523965c583df2617ee2044
Magho <mmaghraby134@gmail.com>
2018-08-26 17:36:52 +0200

    add ajax functions fore add and delete items

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,21 @@
+var bodyParser = require('body-parser');
+var data = [
+	{item : 'study Ml'},
+	{item : 'revise holy qouran'},
+	{item : 'apply project on studied material'},
+	{item : 'go to gym'}
+];
+var urlencodedParser = bodyParser.urlencoded({extended : false});
+
 module.exports = function(app) {
 
 	app.get('/todo', function(req, res){
-		res.render('todo');
+		res.render('todo', {todos : data});
 	});
 
-	app.post('/todo', function(req, res){
-		
+	app.post('/todo', urlencodedParser, function(req, res){
+		data.push(req.body);
+		res.json(data);
 	});
 
 	app.delete('/todo', function(req, res){
