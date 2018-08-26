c81a071bac69dcb1b4b6f43f43ad5c6ff4719f32
Magho <mmaghraby134@gmail.com>
2018-08-26 18:28:12 +0200

    connect to database, create model using mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,32 @@
 var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended : false});
+
+var mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.connect('mongodb://test:test123@ds235022.mlab.com:35022/todo');
+
+//create a schema 
+var todoSchema = new mongoose.Schema({
+	item : String
+});
+
+// create model
+var Todo = mongoose.model('Todo', todoSchema);
+
+// create first item 
+var itemOne = Todo({ item : 'study Ml' }).save(function(err){
+	if (err)throw err;
+	console.log('item saved');
+});
+
+
 var data = [
 	{item : 'study Ml'},
 	{item : 'revise holy qouran'},
 	{item : 'apply project on studied material'},
 	{item : 'go to gym'}
 ];
-var urlencodedParser = bodyParser.urlencoded({extended : false});
 
 module.exports = function(app) {
 
